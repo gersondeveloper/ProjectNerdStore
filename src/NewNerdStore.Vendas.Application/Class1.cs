2e4a13967ed1e089a341c0e0b10bd9bd33619deb
Gerson C Filho <gersoncfilho@mac.com>
2020-08-06 22:42:28 -0300

    first commit

---
+++ src/NewNerdStore.Vendas.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NewNerdStore.Vendas.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/NewNerdStore.Vendas.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NewNerdStore.Vendas.Domain
+{
+    public class Class1
+    {
+    }
+}
